8350adfec8d62640c3cc21b8d5e4d40b4f18f46a
Selamet53 <selamet.zamanoglu@polymtl.ca>
2024-12-03 22:38:59 -0500

    Maximum that I can do lmao

--- source/main.py
+++ source/main.py
@@ -1,8 +1,9 @@
 import asyncio
 import aiohttp
 
+# For testing
 async def call_endpoint_http(session, i):
-    url = "http://54.88.113.8:8000/"
+    url = "http://3.82.44.159:8000/" # replace the ip with the gatekeeper ip
     headers = {'content-type': 'application/json'}
     try:
         print(f"Sending request {i} to {url}")
@@ -13,14 +14,53 @@ async def call_endpoint_http(session, i):
             return status_code, response_json
     except Exception as e:
         return None, str(e)
+    
+# Read data from a table. Exemple: actor
+async def read_data(session, i):
+    url = f"http://3.82.44.159:8000/read/actor" # replace the ip with the gatekeeper ip
+    headers = {'content-type': 'application/json'}
+    try:
+        print(f"Sending read request {i} to {url}")
+        async with session.get(url, headers=headers) as response:
+            status_code = response.status
+            response_json = await response.json()
+            print(f"Read Response: {status_code}, {response_json}")
+            return status_code, response_json
+    except Exception as e:
+        return None, str(e)
+
+# Write data to a table. TODO: Finish the implementation because of errors from manager
+async def write_data(session, i):
+    url = f"http://54.198.231.67:8000/write" # replace the ip with the gatekeeper ip
+    headers = {'content-type': 'application/json'}
+    payload = {
+        "table": "table_name",
+        "column": "column_name",
+        "value": f"value_{i}"
+    }
+    try:
+        print(f"Sending write request {i} to {url}")
+        async with session.post(url, headers=headers, json=payload) as response:
+            status_code = response.status
+            response_json = await response.json()
+            print(f"Write Response: {status_code}, {response_json}")
+            return status_code, response_json
+    except Exception as e:
+        return None, str(e)
 
 async def main():
-    num_requests = 5
+    num_requests = 1000
     print(f"Sending {num_requests} requests to /")
 
     async with aiohttp.ClientSession() as session:
-        tasks = [call_endpoint_http(session, i) for i in range(num_requests)]
-        await asyncio.gather(*tasks)
+        # tasks = [call_endpoint_http(session, i) for i in range(num_requests)]
+        # await asyncio.gather(*tasks)
+
+        read_tasks = [read_data(session, i) for i in range(num_requests)]
+        await asyncio.gather(*read_tasks)
+
+        write_tasks = [write_data(session, i) for i in range(num_requests)]
+        await asyncio.gather(*write_tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- source/scripts.py
+++ source/scripts.py
@@ -1,10 +1,7 @@
 # project: the fast api code is mostly from the first lab assignment but modified for this project's needs.
-mysql = '''#!/bin/bash
+manager = '''#!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install -y mysql-server sysbench unzip python3 python3-pip python3-venv
-
-# Go to ~/
-cd /home/ubuntu
 
 # Setup MySQL
 MYSQL_PASSWORD="root"
@@ -20,16 +17,17 @@ mysql -u root -p"$MYSQL_PASSWORD" -e "SOURCE ./sakila-db/sakila-schema.sql ; SOU
 sudo sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sakila --mysql-user="root" --mysql-password="$MYSQL_PASSWORD" prepare
 sudo sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sakila --mysql-user="root" --mysql-password="$MYSQL_PASSWORD" run
 
-
-# Listening to requests
+# MySQL Node code
+# Setup python environment
 python3 -m venv /home/ubuntu/venv
-
 source /home/ubuntu/venv/bin/activate
 
-pip install fastapi uvicorn
+# Install required packages
+pip install fastapi uvicorn mysql-connector-python
 
 cat <<EOF > /home/ubuntu/main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import mysql.connector
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -37,18 +35,130 @@ logger = logging.getLogger("Cluster")
 
 app = FastAPI()
 
+# MySQL connection config
+db_config = {
+    "user": "root",
+    "password": "root",
+    "host": "127.0.0.1",
+    "database": "sakila",
+}
+
 @app.get("/")
 async def root():
     message = "Instance has received the request"
     logger.info(message)
-    return {"message": message + " from Manager"}
+    return {"message": message + " from Manager Node!"}
+
+@app.get("/read/{table}")
+async def read_data(table: str):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(f"SELECT * FROM {table} LIMIT 10;")
+        results = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return {"data": results}
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to read data")
+
+@app.post("/write") # TODO errors
+async def write_data(table: str, column: str, value: str):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        cursor.execute(f"INSERT INTO {table} ({column}) VALUES (%s);", (value,))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        logger.info(f"Inserted {value} into {table}({column})")
+        return {"message": f"Inserted {value} into {table}({column})"}
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to write data")
 EOF
 
+# Change ownership and permissions
 chown ubuntu:ubuntu /home/ubuntu/main.py
 sleep 5
 
+# Run FastAPI app
 cd /home/ubuntu
-nohup /home/ubuntu/venv/bin/uvicorn  main:app --host 0.0.0.0 --port 8000 > /home/ubuntu/uvicorn.log 2>&1 &
+nohup /home/ubuntu/venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 > /home/ubuntu/uvicorn.log 2>&1 &
+'''
+worker = '''#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install -y mysql-server sysbench unzip python3 python3-pip python3-venv
+
+# Setup MySQL
+MYSQL_PASSWORD="root"
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '$MYSQL_PASSWORD';"
+
+# Setup sakila db
+wget https://downloads.mysql.com/docs/sakila-db.zip
+unzip sakila-db.zip
+
+mysql -u root -p"$MYSQL_PASSWORD" -e "SOURCE ./sakila-db/sakila-schema.sql ; SOURCE ./sakila-db/sakila-data.sql ;"
+
+# Running sysbench
+sudo sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sakila --mysql-user="root" --mysql-password="$MYSQL_PASSWORD" prepare
+sudo sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sakila --mysql-user="root" --mysql-password="$MYSQL_PASSWORD" run
+
+# MySQL Node code
+# Setup python environment
+python3 -m venv /home/ubuntu/venv
+source /home/ubuntu/venv/bin/activate
+
+# Install required packages
+pip install fastapi uvicorn mysql-connector-python
+
+cat <<EOF > /home/ubuntu/main.py
+from fastapi import FastAPI, HTTPException
+import mysql.connector
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Cluster")
+
+app = FastAPI()
+
+# MySQL connection config
+db_config = {
+    "user": "root",
+    "password": "root",
+    "host": "127.0.0.1",
+    "database": "sakila",
+}
+
+@app.get("/")
+async def root():
+    message = "Instance has received the request"
+    logger.info(message)
+    return {"message": message + " from Worker Node!"}
+
+@app.get("/read/{table}")
+async def read_data(table: str):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(f"SELECT * FROM {table} LIMIT 10;")
+        results = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return {"data": results}
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to read data")
+EOF
+
+# Change ownership and permissions
+chown ubuntu:ubuntu /home/ubuntu/main.py
+sleep 5
+
+# Run FastAPI app
+cd /home/ubuntu
+nohup /home/ubuntu/venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 > /home/ubuntu/uvicorn.log 2>&1 &
 '''
 gatekeeper = '''#!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
@@ -76,6 +186,26 @@ async def root():
     target_url = f"http://TRUSTED_HOST_URL:8000"
     
     return RedirectResponse(url=target_url)
+
+@app.get("/read/{table}")
+async def read_data(table: str):
+    try:
+        print("Forwarding Read request to Trusted Host: TRUSTED_HOST_URL")
+        target_url = f"http://TRUSTED_HOST_URL:8000"
+        return RedirectResponse(url=target_url)
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to read data")
+
+@app.post("/write")
+async def write_data(table: str, column: str, value: str):
+    try:
+        print("Forwarding Write request to Trusted Host: TRUSTED_HOST_URL")
+        target_url = f"http://TRUSTED_HOST_URL:8000"
+        return RedirectResponse(url=target_url)
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to write data")
 EOF
 
 chown ubuntu:ubuntu /home/ubuntu/main.py
@@ -108,8 +238,27 @@ app = FastAPI()
 async def root():
     print("Forwarding request to the proxy: PROXY_URL")
     target_url = f"http://PROXY_URL:8000"
-    
     return RedirectResponse(url=target_url)
+
+@app.get("/read/{table}")
+async def read_data(table: str):
+    try:
+        print("Forwarding Read request to the proxy: PROXY_URL")
+        target_url = f"http://PROXY_URL:8000"
+        return RedirectResponse(url=target_url)
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to read data")
+
+@app.post("/write")
+async def write_data(table: str, column: str, value: str):
+    try:
+        print("Forwarding Write request to Trusted Host: PROXY_URL")
+        target_url = f"http://PROXY_URL:8000"
+        return RedirectResponse(url=target_url)
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to write data")
 EOF
 
 chown ubuntu:ubuntu /home/ubuntu/main.py
@@ -144,6 +293,26 @@ async def root():
     target_url = f"http://MANAGER_URL:8000"
     
     return RedirectResponse(url=target_url)
+
+@app.get("/read/{table}")
+async def read_data(table: str):
+    try:
+        print("Forwarding Read request to the proxy: WORKER1_URL")
+        target_url = f"http://WORKER1_URL:8000"
+        return RedirectResponse(url=target_url)
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to read data")
+
+@app.post("/write")
+async def write_data(table: str, column: str, value: str):
+    try:
+        print("Forwarding Write request to Trusted Host: MANAGER_URL")
+        target_url = f"http://MANAGER_URL:8000"
+        return RedirectResponse(url=target_url)
+    except mysql.connector.Error as err:
+        logger.error(f"Error: {err}")
+        raise HTTPException(status_code=500, detail="Failed to write data")
 EOF
 
 chown ubuntu:ubuntu /home/ubuntu/main.py
